67a63832ecd033c440104ec9d00c1dd2ea98f302
primary157 <victorgvbh@gmail.com>
2017-09-29 19:56:57 -0300

    tela de desenho cheia funcional

--- topnav.js
+++ topnav.js
@@ -2,13 +2,13 @@ var toggledTopnav = 0;
 function openTopNav() {
   if (toggledTopnav == 0) {
     document.getElementById("myTopnav").style.height = "0";
-    document.getElementById("main").style.marginTop = "0";
+    //document.getElementById("main").style.marginTop = "40px";
     document.getElementById("mySidenav").style.top = "0";
     document.getElementById("openSideNav").style.top = "70px";
     document.getElementById("openTopNav").style.top = "-106px";
   } else {
     document.getElementById("myTopnav").style.height = "50px";
-    document.getElementById("main").style.marginTop = "50px";
+    //document.getElementById("main").style.marginTop = "90px";
     document.getElementById("mySidenav").style.top = "50px";
     document.getElementById("openSideNav").style.top = "120px";
     document.getElementById("openTopNav").style.top = "-56px";
