0a5b8eebad9cb1c8dc41de814e710684a0390c20
primary157 <victorgvbh@gmail.com>
2017-09-29 17:20:42 -0300

    cabeçalho configurado

--- topnav.js
+++ topnav.js
@@ -1,15 +1,15 @@
 function openTopNav() {
-  if (document.getElementById("myTopnav").style.height != "50px") {
-    document.getElementById("myTopnav").style.height = "50px";
-    document.getElementById("main").style.marginTop = "50px";
-    document.getElementById("mySidenav").style.marginTop = "50px";
-    document.getElementById("openSideNav").style.top = "120px";
-    document.getElementById("TopperNav").style.top = "-56px";
-  } else {
+  if (document.getElementById("TopperNav").style.top != "-106px") {
     document.getElementById("myTopnav").style.height = "0";
     document.getElementById("main").style.marginTop = "0";
-    document.getElementById("mySidenav").style.marginTop = "0";
+    document.getElementById("mySidenav").style.top = "0";
     document.getElementById("openSideNav").style.top = "70px";
     document.getElementById("TopperNav").style.top = "-106px";
+  } else {
+    document.getElementById("myTopnav").style.height = "50px";
+    document.getElementById("main").style.marginTop = "50px";
+    document.getElementById("mySidenav").style.top = "50px";
+    document.getElementById("openSideNav").style.top = "120px";
+    document.getElementById("TopperNav").style.top = "-56px";
   }
 }
