bb169eb48f867497b88c1c0dcd1bab5672dd6f89
primary157 <victorgvbh@gmail.com>
2017-09-29 17:28:36 -0300

    ambas barras funcionam e agora existe variavel de estado

--- sidenav.js
+++ sidenav.js
@@ -1,12 +1,14 @@
+var toggledSidenav = 0;
+
 function openSideNav() {
-  if(document.getElementById("mySidenav").style.width != "250px"){
+  if (toggledSidenav == 0) {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("openSideNav").style.right = "-66px";
+  } else {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginRight = "270px";
     document.getElementById("openSideNav").style.right = "184px";
   }
-  else{
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
-    document.getElementById("openSideNav").style.right = "-66px";
-  }
+  toggledSidenav = !toggledSidenav;
 }
--- topnav.js
+++ topnav.js
@@ -1,15 +1,17 @@
+var toggledTopnav = 0;
 function openTopNav() {
-  if (document.getElementById("TopperNav").style.top != "-106px") {
+  if (toggledTopnav == 0) {
     document.getElementById("myTopnav").style.height = "0";
     document.getElementById("main").style.marginTop = "0";
     document.getElementById("mySidenav").style.top = "0";
     document.getElementById("openSideNav").style.top = "70px";
-    document.getElementById("TopperNav").style.top = "-106px";
+    document.getElementById("openTopNav").style.top = "-106px";
   } else {
     document.getElementById("myTopnav").style.height = "50px";
     document.getElementById("main").style.marginTop = "50px";
     document.getElementById("mySidenav").style.top = "50px";
     document.getElementById("openSideNav").style.top = "120px";
-    document.getElementById("TopperNav").style.top = "-56px";
+    document.getElementById("openTopNav").style.top = "-56px";
   }
+  toggledTopnav = !toggledTopnav;
 }
