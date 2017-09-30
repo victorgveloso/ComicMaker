c785b1f6567bcdaf3b65a18a1b94b5840ba01bde
primary157 <victorgvbh@gmail.com>
2017-09-30 19:52:06 -0300

    classes topoption e sideoption criadas

--- sidenav.js
+++ sidenav.js
@@ -3,11 +3,11 @@ var toggledSidenav = 0;
 function openSideNav() {
   if (toggledSidenav == 0) {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("master").style.marginRight = "40px";
+    //document.getElementById("master").style.marginRight = "40px";
     document.getElementById("openSideNav").style.right = "-66px";
   } else {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("master").style.marginRight = "290px";
+    //document.getElementById("master").style.marginRight = "290px";
     document.getElementById("openSideNav").style.right = "184px";
   }
   toggledSidenav = !toggledSidenav;
