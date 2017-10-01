56b28605215ae98e8076d35b38673ed0b550403d
primary157 <victorgvbh@gmail.com>
2017-10-01 05:07:38 -0300

    Pilha de opcoes implementada, barra de topo com alinhamento central, area de lixo adicionada

--- sidenav.js
+++ sidenav.js
@@ -1,14 +1,32 @@
 var toggledSidenav = 0;
 
-function openSideNav() {
+function openSideNav(i) {
   if (toggledSidenav == 0) {
-    document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("master").style.marginRight = "40px";
-    document.getElementById("openSideNav").style.right = "-66px";
-  } else {
     document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("master").style.marginRight = "40px";
+    switch (i) {
+      case 0:
+        document.getElementById("openSideNavTodos").style.right = "184px";
+        break;
+      case 1:
+        document.getElementById("openSideNavOnomatopeias").style.right = "150px";
+        break;
+      case 2:
+        document.getElementById("openSideNavObjetos").style.right = "180px";
+        break;
+      case 3:
+        document.getElementById("openSideNavBaloes").style.right = "184px";
+        break;
+      default:
+
+    }
+  } else {
+    document.getElementById("mySidenav").style.width = ""; //"250px";
     //document.getElementById("master").style.marginRight = "290px";
-    document.getElementById("openSideNav").style.right = "184px";
+    document.getElementById("openSideNavTodos").style.right = ""; //"184px";
+    document.getElementById("openSideNavObjetos").style.right = "";
+    document.getElementById("openSideNavOnomatopeias").style.right = "";
+    document.getElementById("openSideNavBaloes").style.right = "";
   }
   toggledSidenav = !toggledSidenav;
 }
--- topnav.js
+++ topnav.js
@@ -4,14 +4,20 @@ function openTopNav() {
     document.getElementById("myTopnav").style.height = "0";
     //document.getElementById("main").style.marginTop = "40px";
     document.getElementById("mySidenav").style.top = "0";
-    document.getElementById("openSideNav").style.top = "70px";
+    document.getElementById("openSideNavTodos").style.top = "70px";
+    document.getElementById("openSideNavOnomatopeias").style.top = "200px";
+    document.getElementById("openSideNavObjetos").style.top = "340px";
+    document.getElementById("openSideNavBaloes").style.top = "445px";
     document.getElementById("openTopNav").style.top = "-106px";
   } else {
-    document.getElementById("myTopnav").style.height = "50px";
+    document.getElementById("myTopnav").style.height = "";
     //document.getElementById("main").style.marginTop = "90px";
-    document.getElementById("mySidenav").style.top = "50px";
-    document.getElementById("openSideNav").style.top = "120px";
-    document.getElementById("openTopNav").style.top = "-56px";
+    document.getElementById("mySidenav").style.top = "";
+    document.getElementById("openSideNavTodos").style.top = "";
+    document.getElementById("openSideNavOnomatopeias").style.top = "";
+    document.getElementById("openSideNavObjetos").style.top = "";
+    document.getElementById("openSideNavBaloes").style.top = "";
+    document.getElementById("openTopNav").style.top = "";
   }
   toggledTopnav = !toggledTopnav;
 }
