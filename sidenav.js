a08d33840d84427fd1031bc4b7bc1ce598328c22
primary157 <victorgvbh@gmail.com>
2017-10-01 05:32:57 -0300

    FIXED bug da Pilha de opcoes para telas menores que 600px

--- sidenav.js
+++ sidenav.js
@@ -2,24 +2,45 @@ var toggledSidenav = 0;
 
 function openSideNav(i) {
   if (toggledSidenav == 0) {
-    document.getElementById("mySidenav").style.width = "250px";
     //document.getElementById("master").style.marginRight = "40px";
-    switch (i) {
-      case 0:
-        document.getElementById("openSideNavTodos").style.right = "184px";
-        break;
-      case 1:
-        document.getElementById("openSideNavOnomatopeias").style.right = "150px";
-        break;
-      case 2:
-        document.getElementById("openSideNavObjetos").style.right = "180px";
-        break;
-      case 3:
-        document.getElementById("openSideNavBaloes").style.right = "184px";
-        break;
-      default:
+    if (window.matchMedia('(max-width: 600px)').matches) {
+      document.getElementById("mySidenav").style.width = "125px";
+      switch (i) {
+        case 0:
+          document.getElementById("openSideNavTodos").style.right = "49px";
+          break;
+        case 1:
+          document.getElementById("openSideNavOnomatopeias").style.right = "15px";
+          break;
+        case 2:
+          document.getElementById("openSideNavObjetos").style.right = "45px";
+          break;
+        case 3:
+          document.getElementById("openSideNavBaloes").style.right = "49px";
+          break;
+        default:
 
+      }
+    } else {
+      document.getElementById("mySidenav").style.width = "250px";
+      switch (i) {
+        case 0:
+          document.getElementById("openSideNavTodos").style.right = "184px";
+          break;
+        case 1:
+          document.getElementById("openSideNavOnomatopeias").style.right = "150px";
+          break;
+        case 2:
+          document.getElementById("openSideNavObjetos").style.right = "180px";
+          break;
+        case 3:
+          document.getElementById("openSideNavBaloes").style.right = "184px";
+          break;
+        default:
+
+      }
     }
+
   } else {
     document.getElementById("mySidenav").style.width = ""; //"250px";
     //document.getElementById("master").style.marginRight = "290px";
