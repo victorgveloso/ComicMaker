05690efd04de3b63adb271a83d3338544c869034
CrownKing <32209099+CrownKing@users.noreply.github.com>
2017-10-23 21:32:04 -0200

    balão de texto
     quanado clicar no texto abre a aba de texto, quando clicar na partição abre a paartição

--- floatingwindow.js
+++ floatingwindow.js
@@ -6,3 +6,11 @@ function mostraSeparador() {
   document.getElementById('separadorDeQuadros').style.display = "";
   document.getElementById('separadorDeQuadrosFloatingBoxShadow').style.display = "";
 }
+function texto(){
+  document.getElementById('criadorfloatingBoxShadow').style.display = "";
+  document.getElementById('criadorDeFalas').style.display = "";
+}
+function escondeTexto(){
+  document.getElementById('criadorfloatingBoxShadow').style.display = "none";
+  document.getElementById("criadorDeFalas").style.display = "none"
+}
